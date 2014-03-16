585b4d090ad3af12c000df8fb1befd367648f189
stefangs <stefangs@nethome.nu>
2014-03-16 15:31:28 +0100

    Fixed bug and cleaned up a little bit in the code (a lot more needed)

--- src/main/java/nu/nethome/util/ps/impl/RawDecoder.java
+++ src/main/java/nu/nethome/util/ps/impl/RawDecoder.java
@@ -30,154 +30,163 @@ import java.util.LinkedList;
 
 /**
  * The RawDecoder acts as both a ProtocolDecoder and a ProtocolSampler. It is used to
- * save the raw sample data when a signal is detected in the sample stream. 
+ * save the raw sample data when a signal is detected in the sample stream.
  * When flanks are detected (the parse-method is called) the RawDecoder starts saving
- * all raw samples until the protocol message stops. The raw samples are then 
+ * all raw samples until the protocol message stops. The raw samples are then
  * reported as a special ProtocolMessage which contains all raw sample data.
- * 
- * @author Stefan
  *
+ * @author Stefan
  */
-public class RawDecoder  implements ProtocolDecoder, ProtocolSampler {
-	
-	protected static final int IDLE = 0;
-	protected static final int READING_MESSAGE = 1;
-	/** Maximum size of a raw sampled message in milliseconds */
-	protected static final int MAX_MESSAGE_LENGTH_MS = 800;
-	protected static final int RAW_MESSAGE_END_GAP = 29000; // 11000
-	protected static final int LEVELTIMETOZERO = 1; // X Seconds without signal before level drops to zero
-	protected static final int REPORTSPERSECOND = 10; // Number of levelreports per second
+public class RawDecoder implements ProtocolDecoder, ProtocolSampler {
 
-	protected int m_State = IDLE;
-	protected LinkedList<Integer> m_Pulses;
-	protected boolean m_IsSampling = false;
-	protected ArrayList<Integer> m_Samples;
-	protected int m_SampleCount = 0;
-	protected int m_SampleCountAtLastPulse = 0;
-	protected int m_PulseCount;
-	protected double m_LastPulse = 0;
-	protected ProtocolDecoderSink m_Sink = null;
-	protected int m_SampleFrequency;
-	protected int m_MaxMessageLength;
-	protected boolean m_FreeSampling = false;
-	protected int m_FreeSampleCount = 0;
-	protected int m_Level = 0;
-	protected int m_LevelReportCount = 10;
-	protected LinkedList<Double> m_PulseLengths;
+    private static final int IDLE = 0;
+    private static final int READING_MESSAGE = 1;
+    /**
+     * Maximum size of a raw sampled message in milliseconds
+     */
+    private static final int MAX_MESSAGE_LENGTH_MS = 800;
+    private static final int RAW_MESSAGE_END_GAP = 29000; // 11000
+    private static final int LEVELTIMETOZERO = 1; // X Seconds without signal before level drops to zero
+    private static final int REPORTSPERSECOND = 10; // Number of levelreports per second
+    private static final int MIN_PULSES_PER_SAMPLE = 4;
+
+    private int state = IDLE;
+    private LinkedList<Integer> pulsePositions;
+    private boolean isSampling = false;
+    private ArrayList<Integer> samples;
+    private int sampleCount = 0;
+    private int sampleCountAtLastPulse = 0;
+    private double lastPulseLength = 0;
+    private ProtocolDecoderSink decoderSink = null;
+    private int sampleFrequency;
+    private int maxMessageLength;
+    private boolean isFreeSampling = false;
+    private int signalLevel = 0;
+    private int signalLevelReportCount = 10;
+    private LinkedList<Double> pulseLengths;
     private int maxSampleLength;
 
     public void setTarget(ProtocolDecoderSink sink) {
-		m_Sink = sink;
-	}
+        decoderSink = sink;
+    }
 
-	public RawDecoder() {
-		setSampleRate(22000);
-	}
+    public RawDecoder() {
+        setSampleRate(22000);
+    }
 
-	public ProtocolInfo getInfo() {
-		return new ProtocolInfo("Raw", "Flank Length", "-", 0, 5);
-	}
+    public ProtocolInfo getInfo() {
+        return new ProtocolInfo("Raw", "Flank Length", "-", 0, 5);
+    }
 
-	public void setSampleRate(int frequency) {
-		m_SampleFrequency = frequency;
-		m_MaxMessageLength = (int)(m_SampleFrequency * MAX_MESSAGE_LENGTH_MS * 0.001);
-	}
+    public void setSampleRate(int frequency) {
+        sampleFrequency = frequency;
+        maxMessageLength = (int) (sampleFrequency * MAX_MESSAGE_LENGTH_MS * 0.001);
+    }
 
-	public int getSampleRate() {
-		return m_SampleFrequency;
-	}
+    public int getSampleRate() {
+        return sampleFrequency;
+    }
 
-	/**
-	 * Forces the class to start saving raw data for the specified number of samples.
-	 * When the samples are collected they are reported as usual via the
-	 * ProtocolDecoderSink.
-	 * @param samples Number of samples to collect.
-	 */
-	public void startFreeSampling(int samples) {
-		m_FreeSampleCount = samples;
-        m_FreeSampling = true;
+    /**
+     * Forces the class to start saving raw data for the specified number of samples.
+     * When the samples are collected they are reported as usual via the
+     * ProtocolDecoderSink.
+     *
+     * @param samples Number of samples to collect.
+     */
+    public void startFreeSampling(int samples) {
+        isFreeSampling = true;
         restartSampler(samples);
-	}
+    }
 
     private void restartSampler(int samples) {
-        m_SampleCount = 0;
-        m_Pulses = new LinkedList<Integer>();
-        m_Samples = new ArrayList<Integer>(samples+ 1);
-        m_PulseLengths = new LinkedList<Double>();
+        sampleCount = 0;
+        pulsePositions = new LinkedList<Integer>();
+        this.samples = new ArrayList<Integer>(samples + 1);
+        pulseLengths = new LinkedList<Double>();
         maxSampleLength = samples;
     }
 
-	public void addSample(int sample) {
+    public void addSample(int sample) {
         calculateSignalLevel(sample);
-		if (m_IsSampling || m_FreeSampling) {
-			m_Samples.add(sample);
-			m_SampleCount++;
-            if (m_SampleCount >= maxSampleLength) {
-                endMessage(!m_FreeSampling);
+        if (isSampling || isFreeSampling) {
+            samples.add(sample);
+            sampleCount++;
+            if (sampleCount >= maxSampleLength) {
+                boolean trimEnd = !isFreeSampling;
+                ReportMessage(trimEnd);
+                stopSampler();
             }
-		}
-	}
-
-    private void calculateSignalLevel(int sample) {
-        int absSample = Math.abs(sample);
-        if (m_Level < absSample) m_Level = absSample;
-        m_LevelReportCount--;
-        if (m_LevelReportCount <= 0){
-            m_Sink.reportLevel(m_Level);
-            m_Level -= 127 / (REPORTSPERSECOND * LEVELTIMETOZERO);
-            m_LevelReportCount = m_SampleFrequency/REPORTSPERSECOND;
         }
     }
 
-    public int parse(double pulse, boolean state) {
-		switch (m_State) {
-			case IDLE: {
-				if ((pulse > 0.0) && (pulse < 200000.0) && !state){
-                    if (!m_FreeSampling) {
-                        restartSampler(m_MaxMessageLength);
+    private void calculateSignalLevel(int sample) {
+        int absSample = Math.abs(sample);
+        if (signalLevel < absSample) signalLevel = absSample;
+        signalLevelReportCount--;
+        if (signalLevelReportCount <= 0) {
+            decoderSink.reportLevel(signalLevel);
+            signalLevel -= 127 / (REPORTSPERSECOND * LEVELTIMETOZERO);
+            signalLevelReportCount = sampleFrequency / REPORTSPERSECOND;
+        }
+    }
+
+    public int parse(double pulseLength, boolean isMarkPulse) {
+        switch (state) {
+            case IDLE: {
+                if ((pulseLength > 0.0) && (pulseLength < 200000.0) && !isMarkPulse) {
+                    if (!isFreeSampling) {
+                        restartSampler(maxMessageLength);
                     }
-					m_PulseCount = 1;
-					m_IsSampling = true;
-					m_Pulses.add(m_SampleCount);
-					m_PulseLengths.add(pulse);
-					m_State = READING_MESSAGE;
-				}
-				break;
-			}
-			case READING_MESSAGE: {
-				if ((pulse > 0) && ((pulse < RAW_MESSAGE_END_GAP) || m_FreeSampling)) {
-					m_Pulses.add(m_SampleCount);
-					m_PulseLengths.add(pulse);
-					m_PulseCount++;
-				}
-				else if (m_PulseCount > 1 && pulse > 0){
-					// It has been a long space, so we got our message.
-					endMessage(true);
-				}
-				break;
-			}
-		}
-		m_LastPulse = pulse;
-		m_SampleCountAtLastPulse = m_SampleCount;
-        return m_State;
-	}
-	
-	/**
-	 * End sampling of a raw message and report the collected message
-     * @param trimEnd
-     */
-	protected void endMessage(boolean trimEnd) {
-		// First we trim  off the space from the samples
-		for (int i = m_Samples.size() - 1; trimEnd && (i > m_SampleCountAtLastPulse); i--){
-			m_Samples.remove(i);
-		}
-		RawProtocolMessage message = new RawProtocolMessage(m_Pulses, m_Samples, m_SampleFrequency, m_PulseLengths);
-		// Report the parsed message
-		m_Sink.parsedMessage(message);
-		m_IsSampling = false;
-        m_FreeSampling = false;
-        m_FreeSampleCount = 0;
-		m_State = IDLE;
-		System.out.println("Raw sample with " + Integer.toString(m_SampleCount) + " samples");
-	}
+                    isSampling = true;
+                    addPulse(pulseLength);
+                    state = READING_MESSAGE;
+                }
+                break;
+            }
+            case READING_MESSAGE: {
+                if ((pulseLength == 0) && isMarkPulse && (pulsePositions.size() > 0)) {
+                    removeLastSpacePulseToBeReplacedByNext();
+                } else if ((pulseLength < RAW_MESSAGE_END_GAP) || isFreeSampling) {
+                    addPulse(pulseLength);
+                } else {
+                    // It has been a long space, so we got our message.
+                    if (pulsePositions.size() >= MIN_PULSES_PER_SAMPLE && pulseLength > 0) {
+                        ReportMessage(true);
+                    }
+                    stopSampler();
+                }
+            }
+            break;
+        }
+        lastPulseLength = pulseLength;
+        sampleCountAtLastPulse = sampleCount;
+        return state;
+    }
+
+    private void removeLastSpacePulseToBeReplacedByNext() {
+        pulsePositions.removeLast();
+        pulseLengths.removeLast();
+    }
+
+    private void addPulse(double pulse) {
+        pulsePositions.add(sampleCount);
+        pulseLengths.add(pulse);
+    }
+
+    private void ReportMessage(boolean trimEnd) {
+        // First we trim  off the space from the samples
+        for (int i = samples.size() - 1; trimEnd && (i > sampleCountAtLastPulse); i--) {
+            samples.remove(i);
+        }
+        RawProtocolMessage message = new RawProtocolMessage(pulsePositions, samples, sampleFrequency, pulseLengths);
+        // Report the parsed message
+        decoderSink.parsedMessage(message);
+    }
+
+    private void stopSampler() {
+        isSampling = false;
+        isFreeSampling = false;
+        state = IDLE;
+    }
 }

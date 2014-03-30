a74c30e9708facfbb659ab4c84c5483fc5dcdd03
stefangs <stefangs@nethome.nu>
2014-03-30 22:00:13 +0200

    Added BitString class

---
+++ src/main/java/nu/nethome/util/ps/BitString.java
@@ -0,0 +1,106 @@
+package nu.nethome.util.ps;
+
+import org.apache.commons.lang3.Validate;
+
+/**
+ * Represents a string of bits.
+ */
+public class BitString {
+    private static final int MAX_LENGTH = 64;
+    private int length;
+    private long bits;
+
+    public BitString(long bits, int length) {
+        this.bits = bits;
+        this.length = length;
+    }
+
+    public BitString(int length) {
+        Validate.inclusiveBetween(0, MAX_LENGTH, length);
+        this.length = length;
+    }
+
+    public BitString() {
+        length = 0;
+    }
+
+    public int length() {
+        return length;
+    }
+
+    public void clear() {
+        length = 0;
+    }
+
+    public boolean getBit(int position) {
+        Validate.inclusiveBetween(0, length - 1, position);
+        return ((bits >> position) & 1) == 1;
+    }
+
+    public void setBit(int position, boolean value) {
+        Validate.inclusiveBetween(0, MAX_LENGTH, length);
+        if (position >= length) {
+            length = position + 1;
+        }
+        long bitMask = 1L << position;
+        if (value) {
+            bits |= bitMask;
+        } else {
+            bits &= ~bitMask;
+        }
+    }
+
+    public void addMsb(boolean msb) {
+        Validate.isTrue(length < MAX_LENGTH);
+
+        setBit(length, msb);
+    }
+
+    public void addLsb(boolean lsb) {
+        Validate.isTrue(length < MAX_LENGTH);
+
+        bits <<= 1;
+        bits |= lsb ? 1 : 0;
+        length++;
+    }
+
+    public int extractInt(Field position) {
+        Validate.isTrue(position.startBit < MAX_LENGTH);
+        Validate.isTrue(position.startBit + position.length < MAX_LENGTH);
+        long result = bits;
+        result >>= position.startBit;
+        result &= (1 << (position.startBit + position.length)) - 1;
+        return (int)result;
+    }
+
+    public void insert(Field position, int value) {
+        Validate.isTrue(position.startBit + position.length < MAX_LENGTH);
+        if (position.startBit + position.length > length) {
+            length = position.startBit + position.length;
+        }
+        long clearMask = ((1L << position.length) - 1);
+        long valueMask = value & clearMask;
+
+        valueMask <<= position.startBit;
+        clearMask <<= position.startBit;
+        clearMask = ~clearMask;
+        bits &= clearMask;
+        bits |= valueMask;
+    }
+
+    /**
+     * Represents a field of bits within a bit string. The field is defined as start bit position (inclusive) and
+     * a length of the field.
+     */
+    public static class Field {
+        public final int startBit;
+        public final int length;
+
+        public Field(int startBit, int length) {
+            Validate.isTrue(startBit >= 0);
+            Validate.isTrue(length > 0);
+            this.startBit = startBit;
+            this.length = length;
+        }
+    }
+}
---
+++ src/test/java/nu/nethome/util/ps/BitStringTest.java
@@ -0,0 +1,109 @@
+package nu.nethome.util.ps;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ *
+ */
+public class BitStringTest {
+
+    BitString bitString;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorValidatesUpper() throws Exception {
+        bitString = new BitString(65);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorValidatesLower() throws Exception {
+        bitString = new BitString(-1);
+    }
+
+    @Test
+    public void constructorSetsAllZero() throws Exception {
+        bitString = new BitString(64);
+
+        assertThat(bitString.length(), is(64));
+        for (int i = 0; i < 64; i++) {
+            assertThat(bitString.getBit(i), is(false));
+        }
+    }
+
+    @Test
+    public void setsBit() throws Exception {
+        bitString = new BitString(2);
+
+        bitString.setBit(1, true);
+        assertThat(bitString.getBit(0), is(false));
+        assertThat(bitString.getBit(1), is(true));
+        bitString.setBit(1, false);
+        assertThat(bitString.getBit(0), is(false));
+        assertThat(bitString.getBit(1), is(false));
+    }
+
+    @Test
+    public void setBitGrowsString() throws Exception {
+        bitString = new BitString(0);
+
+        assertThat(bitString.length(), is(0));
+        bitString.setBit(17, false);
+        assertThat(bitString.length(), is(18));
+    }
+
+    @Test
+    public void addMsb() throws Exception {
+        bitString = new BitString(1);
+
+        bitString.addMsb(true);
+        assertThat(bitString.getBit(0), is(false));
+        assertThat(bitString.getBit(1), is(true));
+        assertThat(bitString.length(), is(2));
+    }
+
+    @Test
+    public void addLsb() throws Exception {
+        bitString = new BitString(2);
+        bitString.setBit(1, true);
+        bitString.addLsb(true);
+        assertThat(bitString.length(), is(3));
+        assertThat(bitString.getBit(0), is(true));
+        assertThat(bitString.getBit(1), is(false));
+        assertThat(bitString.getBit(2), is(true));
+    }
+
+    @Test
+    public void extractInteger() throws Exception {
+        bitString = new BitString(10);
+        bitString.setBit(5, true);
+
+        assertThat(bitString.extractInt(new BitString.Field(5, 5)), is(1));
+        assertThat(bitString.extractInt(new BitString.Field(0, 6)), is(32));
+    }
+
+    @Test
+    public void insertInteger() throws Exception {
+        bitString = new BitString(10);
+        bitString.insert(new BitString.Field(5, 5), 5);
+
+        assertThat(bitString.extractInt(new BitString.Field(5, 5)), is(5));
+        assertThat(bitString.extractInt(new BitString.Field(0, 10)), is( 5 * 32));
+    }
+
+    @Test
+    public void insertIntegerGrowsString() throws Exception {
+        bitString = new BitString(1);
+        bitString.insert(new BitString.Field(5, 5), 5);
+
+        assertThat(bitString.length(), is(10));
+        assertThat(bitString.extractInt(new BitString.Field(5, 5)), is(5));
+    }
+}
